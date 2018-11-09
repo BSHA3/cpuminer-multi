5f9c574ecbc30a2724f51a136644dcb37abba7e3
Jon Layton <me@jonl.io>
2018-11-09 07:26:58 -0600

    Adjust keccak to sha3d, make it connect to a local stratum pool by default (in cpuminer-conf.json)

--- algo/keccak.c
+++ algo/keccak.c
@@ -14,6 +14,10 @@ extern void keccakhash(void *state, const void *input)
     sph_keccak256 (&ctx_keccak,input, 80);
     sph_keccak256_close(&ctx_keccak, hash);
 
+    sph_keccak256_init(&ctx_keccak);
+    sph_keccak256 (&ctx_keccak,input, 32);
+    sph_keccak256_close(&ctx_keccak, hash);
+
 	memcpy(state, hash, 32);
 }
 
